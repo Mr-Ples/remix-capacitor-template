a8b447e4e0db643c9953e7bb5356f0766d1db461
Mr-Ples <simon.lombard000@gmail.com>
2026-02-12 13:45:59 +0100

    fix

--- scripts/android-build.sh
+++ scripts/android-build.sh
@@ -1,6 +1,8 @@
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 unset ANDROID_SDK_ROOT # needed if you have multiple android sdks installed
+npx cap add android
+npx @capacitor/assets generate
 npm run build
 npx cap sync
 npx cap run android
